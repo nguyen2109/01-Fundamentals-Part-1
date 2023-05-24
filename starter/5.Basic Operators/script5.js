c964f20bce271fa6972ad0f24ed2bb949b079cce
“nguyen2109” <n21091999@gmail.com>
2023-05-24 15:19:10 +0700

    5.Basic Operators - Toán tử cơ bản

--- starter/5.Basic Operators/script5.js
+++ starter/5.Basic Operators/script5.js
@@ -1,3 +1,4 @@
+// Toán tử toán học
 const thisYear = 2023;
 const myYearBirthday = 1999;
 const someBirthday = 1998;
@@ -12,3 +13,18 @@ const firstName = 'Đào';
 const lastName = 'Nguyên';
 
 console.log(firstName + ' ' + lastName); // = Đào Nguyên
+
+// toán tử gán
+let x = 10 + 5; // 15
+x += 10; // x = x + 10
+x -= 10; // x = x - 10
+x /= 10; // x = x / 10
+x *= 10; // x = x * 10
+x **= 10; // x = x mũ 10 ( x = x ** 10)
+x++; // x = x + 1
+x--; // x = x - 1
+console.log(x);
+
+// toán tử so sánh
+// >,<,>=,<=
+console.log(myAge > someAge); // false
