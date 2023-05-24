befa6e59ed949d8f9593e0796a5a39522c943a6a
“nguyen2109” <n21091999@gmail.com>
2023-05-24 12:53:13 +0700

    3.Data types

---
+++ starter/3.Data types/script.js
@@ -0,0 +1,7 @@
+// 1. Number: Floating point numbers 👉 Used for decimals and integers
+// 2. String: Sequence of characters 👉 Used for text
+// 3. Boolean: Logical type that can only be true or false 👉 Used for taking decisions
+// 4. Undefined: Value taken by a variable that is not yet defined (‘empty value’)
+// 5. Null: Also means ‘empty value’
+// 6. Symbol (ES2015): Value that is unique and cannot be changed [Not useful for now]
+// 7. BigInt (ES2020): Larger integers than the Number type can hold
