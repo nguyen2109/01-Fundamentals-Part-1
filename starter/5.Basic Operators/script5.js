19ff312ce7a427d1efb5097b7f63d4333257c4fc
“nguyen2109” <n21091999@gmail.com>
2023-05-24 14:54:15 +0700

    5.Basic Operators - Toán tử cơ bản

---
+++ starter/5.Basic Operators/script5.js
@@ -0,0 +1,14 @@
+const thisYear = 2023;
+const myYearBirthday = 1999;
+const someBirthday = 1998;
+const myAge = thisYear - myYearBirthday;
+const someAge = thisYear - someBirthday;
+
+console.log(myAge); // =24
+console.log(2 ** 3); // 2 mũ 3 = 8
+console.log((myAge * 2) / (someAge / 2) + thisYear); // =2026.84
+
+const firstName = 'Đào';
+const lastName = 'Nguyên';
+
+console.log(firstName + ' ' + lastName); // = Đào Nguyên
